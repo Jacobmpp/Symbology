5983f780311f359ceaa2afaa9638e5506f3f6b15
Jacob Holz <jakeholz03@gmail.com>
2022-09-22 11:45:40 -0500

    A project to start with

---
+++ app/src/main/java/com/symbology/symbology/ui/reflow/ReflowViewModel.java
@@ -0,0 +1,19 @@
+package com.symbology.symbology.ui.reflow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ReflowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ReflowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is reflow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
